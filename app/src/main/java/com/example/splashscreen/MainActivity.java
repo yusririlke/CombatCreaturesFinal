144c71ba63f0b053100f564170bb7028f96822f8
Yusri Zafri <yusri_zafri@gmail.com>
2022-04-24 20:39:39 -0400

    Initial commit

---
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startClick(View view) {
+
+        Intent intent = new Intent(this,MenuActivity.class);
+        startActivity(intent);
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/splashscreen/MenuActivity.java
@@ -0,0 +1,33 @@
+package com.example.splashscreen;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MenuActivity extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        btn = findViewById(R.id.newGameBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this,TS1.class);
+                startActivity(intent);
+            }
+        });
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+
+    }
+
+}
